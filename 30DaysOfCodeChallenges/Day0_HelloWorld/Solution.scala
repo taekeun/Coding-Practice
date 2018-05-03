6b7f81ee1a663bd90d8fd134308125229769892a
taekeun <ytkkty@gmail.com>
2018-05-03 13:34:27 +0900

    scala helloworld

---
+++ 30DaysOfCodeChallenges/Day0_HelloWorld/Solution.scala
@@ -0,0 +1,11 @@
+// scalac Solution.scala
+// scala -classpath . Solution
+// https://docs.scala-lang.org/ko/tutorials/scala-for-java-programmers.html
+
+object Solution {
+  def main(args: Array[String]) {
+    print("Hello, World.")
+    val s = scala.io.StdIn.readLine()
+    println(s)
+  }
+}
