067125f54d366c17e71a5594aacc0892852d7a9f
taekeun <ytkkty@gmail.com>
2018-05-03 13:38:33 +0900

    fix] scala hello, world

--- 30DaysOfCodeChallenges/Day0_HelloWorld/Solution.scala
+++ 30DaysOfCodeChallenges/Day0_HelloWorld/Solution.scala
@@ -4,8 +4,13 @@
 
 object Solution {
   def main(args: Array[String]) {
-    print("Hello, World.")
+    // Print "Hello, World."
+    println("Hello, World.")
+    
+    // Read a string variable
     val s = scala.io.StdIn.readLine()
+
+    // Print the value of the string variable
     println(s)
   }
 }
