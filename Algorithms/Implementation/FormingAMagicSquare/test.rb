fb24747d2172d53f5dd1d4d7eed20b5a1fc215d9
taekeun <ytkkty@gmail.com>
2018-05-26 06:37:48 +0900

    forming a magic square] 첫번째 코드 리팩토링 : uniq 써먹음

--- Algorithms/Implementation/FormingAMagicSquare/test.rb
+++ Algorithms/Implementation/FormingAMagicSquare/test.rb
@@ -7,55 +7,29 @@ require 'test/unit'
 class MagicSquare
   attr_accessor :min_cost, :square
 
-  SAMPLE_A = [[4, 9, 2],
-              [3, 5, 7],
-              [8, 1, 6]]
-  SAMPLE_B = [[4, 9, 2],
-              [3, 5, 7],
-              [8, 1, 6]]
+  SAMPLE = [4, 9, 2, 3, 5, 7, 8, 1, 6]
 
   def cost? (raw, result)
     cost = 0
-    3.times do |i|
-      3.times.map { |j| cost += (raw[i][j] - result[i][j]).abs }
-    end
+    9.times { |i| cost += (raw[i] - result[i]).abs }
     cost
   end
 
-  def magic_square?(square)
-    used_numbers = Array.new(9)
-    square.length.times do |i|
-      square[i].length.times do |j|
-        number = square[i][j]
-        return false unless used_numbers[number - 1].nil?
-        used_numbers[number - 1] = number
-      end
-    end
+  def magic_square?(array)
+    return false unless array.uniq.length == array.length
 
-    number = square[0][0] + square[1][1] + square[2][2]
-    return false if number != square[0][2] + square[1][1] + square[2][0]
+    sum = array[0] + array[4] + array[8]
+    return false unless sum == array[2] + array[4] + array[6]
     3.times do |i|
-      return false if number != square[i].sum
-      return false if number != square[0][i] + square[1][i] + square[2][i]
+      return false unless sum == array[i*3, 3].sum
+      return false unless sum == (array[i] + array[i + 3] + array[i + 3*2])
     end
     true
   end
 
-  def clone_square(square)
-    cloned = Array.new(square.length)
-    square.length.times {|n| cloned[n] = square[n].clone}
-    cloned
-  end
-
   def initialize(square = [])
     @square = square
-    @min_cost = if square.empty?
-                  999
-                else
-                  cost_a = cost?(square, SAMPLE_A)
-                  cost_b = cost?(square, SAMPLE_B)
-                  cost_a < cost_b ? cost_a : cost_b
-                end
+    @min_cost = square.empty? ? 999 : cost?(square, SAMPLE)
   end
 
   def calc_minimum_cost(position, cost, square, remain_numbers)
@@ -64,15 +38,17 @@ class MagicSquare
     return if position > 8
     return if remain_numbers.empty?
 
-    row = position / 3
-    column = position % 3
-    before = square[row][column]
-
+    before = square[position]
     remain_numbers.length.times do |n|
       after = remain_numbers[n]
-      square[row][column] = after
+      square[position] = after
 
-      calc_minimum_cost(position + 1, cost + (before - after).abs, clone_square(square), remain_numbers.clone.tap{|rn| rn.delete_at(n)})
+      calc_minimum_cost(
+        position + 1,
+        cost + (before - after).abs,
+        square.clone,
+        remain_numbers.clone.tap{ |rn| rn.delete_at(n) }
+      )
     end
   end
 
@@ -83,8 +59,8 @@ end
 
 
 def formingMagicSquare(raw)
-  magic_square = MagicSquare.new(raw)
-  magic_square.calc_minimum_cost(0, 0, raw, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+  magic_square = MagicSquare.new(raw.flatten)
+  magic_square.calc_minimum_cost(0, 0, raw.flatten, [1, 2, 3, 4, 5, 6, 7, 8, 9])
   magic_square.min_cost
 end
 
@@ -111,18 +87,18 @@ class TestSolution < Test::Unit::TestCase
   end
 
   def test_cost
-    assert_equal(@sample_a[:cost], MagicSquare.new.cost?(@sample_a[:raw], @sample_a[:fix]))
-    assert_equal(@sample_b[:cost], MagicSquare.new.cost?(@sample_b[:raw], @sample_b[:fix]))
+    assert_equal(@sample_a[:cost], MagicSquare.new.cost?(@sample_a[:raw].flatten, @sample_a[:fix].flatten))
+    assert_equal(@sample_b[:cost], MagicSquare.new.cost?(@sample_b[:raw].flatten, @sample_b[:fix].flatten))
   end
 
   def test_magic_square?
-    assert_false(MagicSquare.new.magic_square?(@sample_a[:raw]))
-    assert_true(MagicSquare.new.magic_square?(@sample_a[:fix]))
-    assert_false(MagicSquare.new.magic_square?(@sample_b[:raw]))
-    assert_true(MagicSquare.new.magic_square?(@sample_b[:fix]))
+    assert_false(MagicSquare.new.magic_square?(@sample_a[:raw].flatten))
+    assert_true(MagicSquare.new.magic_square?(@sample_a[:fix].flatten))
+    assert_false(MagicSquare.new.magic_square?(@sample_b[:raw].flatten))
+    assert_true(MagicSquare.new.magic_square?(@sample_b[:fix].flatten))
     assert_false(MagicSquare.new.magic_square?([[2, 2, 2],
-                                [2, 2, 2],
-                                [2, 2, 2]]))
+                                                [2, 2, 2],
+                                                [2, 2, 2]].flatten))
   end
 
   def test_result
