3ccdab0e00e5326d6e6b4f0ff0a4d7c4f7b188fb
taekeun <ytkkty@gmail.com>
2018-08-20 14:40:58 +0900

    simple refactoring

--- 30DaysOfCodeChallenges/Day2_Operators/scala/Solution.scala
+++ 30DaysOfCodeChallenges/Day2_Operators/scala/Solution.scala
@@ -4,19 +4,20 @@ import java.io._
 
 object Solution {
   def totalCost(mealCost: Double, tipPercent: Int, taxPercent: Int): Int = {
-    def calcPercent(percent: Int): Double = {
-      mealCost * percent / 100
-    }
+    def calcPercent(percent: Int): Double = mealCost * percent / 100
+
     val cost = mealCost + calcPercent(tipPercent) + calcPercent(taxPercent)
     math.round(cost).toInt
   }
 
-  def main(args: Array[String]) {
+  def main(args: Array[String]):Unit = {
     val stdin = scala.io.StdIn
     val mealCost = stdin.readLine.trim.toDouble
     val tipPercent = stdin.readLine.trim.toInt
     val taxPercent = stdin.readLine.trim.toInt
+
     val result = totalCost(mealCost, tipPercent, taxPercent)
-    println("The total meal cost is " + result + " dollars.")
+
+    println(s"The total meal cost is $result dollars.")
   }
 }
