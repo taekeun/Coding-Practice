17291ab65a22ad077c3d5c37bdd288f0dd7e68bf
taekeun <ytkkty@gmail.com>
2021-08-09 17:20:35 +0900

    5 Simple Pig Latin: 리팩토링. 단어 구분

--- Codewars/5_simple_pig_latin/ruby_solution.rb
+++ Codewars/5_simple_pig_latin/ruby_solution.rb
@@ -3,14 +3,15 @@
 # https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/ruby
 # Simple Pig Latin
 
-def changer(t)
-  f = t[0]
-  return t unless f.match?(/[a-zA-Z]/)
-
-  l = t[1..-1]
-  l.concat(f, "ay")
-end
+# def changer(t)
+#   f = t[0]
+#   return t unless f.match?(/[a-zA-Z]/)
+#
+#   l = t[1..-1]
+#   l.concat(f, "ay")
+# end
 
 def pig_it text
-  text.split.map { |t| changer(t) }.join(" ")
+  text.split.map { |t| t.match?(/\w/) ? "#{t[1..-1]}#{t[0]}ay" : t }.join(" ")
+  # text.split.map { |t| changer(t) }.join(" ")
 end
