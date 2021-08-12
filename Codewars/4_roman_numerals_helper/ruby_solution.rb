bd51a39cae44a7c683babc2afef6a45cbd23dc17
taekeun <ytkkty@gmail.com>
2021-08-12 16:24:31 +0900

    4 Roman Numerals Helper: 쉽게 가자.

--- Codewars/4_roman_numerals_helper/ruby_solution.rb
+++ Codewars/4_roman_numerals_helper/ruby_solution.rb
@@ -13,7 +13,9 @@
 # D	500
 # M	1000
 class RomanNumerals
-  SYMBOLS = { M: 1000, D:500, C:100, L:50, X:10, V:5, I:1 }
+  SYMBOLS = { M: 1000, CM: 900, D:500, CD: 400,
+              C: 100, XC: 90, L:50, XL: 40,
+              X:10, IX: 9, V:5, IV: 4, I:1 }
   class << self
     def to_roman(num)
       to_r(num, 0)
