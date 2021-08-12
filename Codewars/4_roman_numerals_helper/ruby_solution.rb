c6afa58ae0029f662cb98e9469ab27ec37dee8d4
taekeun <ytkkty@gmail.com>
2021-08-12 16:42:57 +0900

    4 Roman Numerals Helper: local test passed.

--- Codewars/4_roman_numerals_helper/ruby_solution.rb
+++ Codewars/4_roman_numerals_helper/ruby_solution.rb
@@ -13,16 +13,16 @@
 # D	500
 # M	1000
 class RomanNumerals
-  SYMBOLS = { M: 1000, CM: 900, D:500, CD: 400,
-              C: 100, XC: 90, L:50, XL: 40,
-              X:10, IX: 9, V:5, IV: 4, I:1 }
+  SYMBOLS = { 'M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400,
+              'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40,
+              'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1 }
   class << self
     def to_roman(num)
       to_r(num, 0)
     end
 
     def from_roman(roman)
-      # TODO
+      from_r(roman)
     end
 
     def to_r(num, i)
@@ -30,7 +30,14 @@ class RomanNumerals
 
       n = SYMBOLS.values[i]
       s = SYMBOLS.key(n)
-      s.to_s * (num / n) + to_r(num % n, i + 1)
+      s * (num / n) + to_r(num % n, i + 1)
+    end
+
+    def from_r(roman)
+      return 0 if roman.empty?
+
+      rs = SYMBOLS[roman[0]] < SYMBOLS[roman[1]].to_i ? roman[0..1] : roman[0]
+      SYMBOLS[rs] + from_r(roman[rs.length..])
     end
   end
 end
