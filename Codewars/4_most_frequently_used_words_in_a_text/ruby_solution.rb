c5f6fa2344977d2414b9fd7f7c9d23db39441e08
taekeun <ytkkty@gmail.com>
2021-08-13 16:11:45 +0900

    4 Most Words : 커뮤니티 참고 후 리팩토링. - scan 이 있다. - Hash 만들때 기본값 0으로 할 수 있다.

--- Codewars/4_most_frequently_used_words_in_a_text/ruby_solution.rb
+++ Codewars/4_most_frequently_used_words_in_a_text/ruby_solution.rb
@@ -11,11 +11,14 @@ def top_3_words(text)
   # 앞, 뒤에 있는 기호는 무시한다.
   # ' 는 몇개라도 봐준다.
   # 중간에 있는 기호는 모르겠는데 일단 그냥 가자.
-  words = {}
-  text.downcase.split.each do |w|
-    w = w.match(/\w+(?:'*)\w*/).to_s
-    next if w.empty?
-    words[w] ? words[w] += 1 : words[w] = 1
-  end
-  words.sort_by{|_, v| -v}[0..2].map{|x| x.first }
+
+  # words = {}
+  # text.downcase.split.each do |w|
+  #   w = w.match(/\w+(?:'*)\w*/).to_s
+  #   next if w.empty?
+  #   words[w] ? words[w] += 1 : words[w] = 1
+  # end
+  # words.sort_by{|_, v| -v}[0..2].map{|x| x.first }
+
+  text.downcase.scan(/\w+(?:'*)\w*/).inject(Hash.new(0)){|h, w| h[w] += 1; h}.sort_by{|_, v| -v}[0..2].map{|x| x.first }
 end
